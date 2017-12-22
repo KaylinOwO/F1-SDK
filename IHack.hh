c68ce1ae9fd72ccd5f3a4af58aebacbf4bda68da
F1ssi0N <josh33901@gmail.com>
2017-12-22 10:15:04 +0000

    Update code formatting and some indexes

--- IHack.hh
+++ IHack.hh
@@ -15,104 +15,104 @@ class IHack
 {
 
 private:
-	Derived &derived ()
-	{
-		return static_cast<Derived &> (*this);
-	}
+    Derived &derived()
+    {
+        return static_cast<Derived &>(*this);
+    }
 
 public:
-	~IHack ()
-	{
-	}
+    ~IHack()
+    {
+    }
 
-	// These functions must be defined in doubles to allow the derived class
-	// to not implement them
-	// if they were not in this double format then there would be boundless recursion (which is caught as a compiler error)
+    // These functions must be defined in doubles to allow the derived class
+    // to not implement them
+    // if they were not in this double format then there would be boundless recursion (which is caught as a compiler warning)
 
-	// TODO: remove
-	const char *_name () const
-	{
-		return ""
-	}
+    // TODO: remove
+    const char *_name() const
+    {
+        return ""
+    }
 
-	// called on init
-	void _init ()
-	{
-		return derived ().init ();
-	}
-	void init ()
-	{
-		return;
-	}
+    // called on init
+    void _init()
+    {
+        return derived().init();
+    }
+    void init()
+    {
+        return;
+    }
 
-	// called during paint traverse
-	bool _paint ()
-	{
-		derived ().paint ();
-		return false;
-	}
-	bool paint ()
-	{
-		return false;
-	}
+    // called during paint traverse
+    bool _paint()
+    {
+        derived().paint();
+        return false;
+    }
+    bool paint()
+    {
+        return false;
+    }
 
-	void _processCommandBeforePred (CUserCmd *pCommand)
-	{
-		return derived ().processCommandBeforePred (pCommand);
-	}
-	void processCommandBeforePred (CUserCmd *pCommand)
-	{
-		return;
-	}
+    void _processCommandBeforePred(CUserCmd *user_cmd)
+    {
+        return derived().processCommandBeforePred(user_cmd);
+    }
+    void processCommandBeforePred(CUserCmd *user_cmd)
+    {
+        return;
+    }
 
-	// called during clientmode createmove - takes a CUserCMD
-	void _processCommand (CUserCmd *pUserCmd)
-	{
-		return derived ().processCommand (pUserCmd);
-	}
-	void processCommand (CUserCmd *pUserCmd)
-	{
-		return;
-	}
+    // called during clientmode createmove - takes a CUserCMD
+    void _processCommand(CUserCmd *user_cmd)
+    {
+        return derived().processCommand(user_cmd);
+    }
+    void processCommand(CUserCmd *user_cmd)
+    {
+        return;
+    }
 
-	// called during keyevent
-	bool _keyEvent (ButtonCode_t keynum)
-	{
-		return derived ().keyEvent (keynum);
-	}
-	bool keyEvent (ButtonCode_t keynum)
-	{
-		return;
-	}
+    // called during keyevent
+    bool _keyEvent(ButtonCode_t keynum)
+    {
+        return derived().keyEvent(keynum);
+    }
+    bool keyEvent(ButtonCode_t keynum)
+    {
+        return;
+    }
 
-	// the entity here will not be the local player
-	// and it will not be null
-	void _processEntity (CBaseEntity *pBaseEntity)
-	{
-		return derived ().processEntity (pBaseEntity);
-	}
-	void processEntity (CBaseEntity *pBaseEntity)
-	{
-		return;
-	}
+    // the entity here will not be the local player
+    // and it will not be null
+    void _processEntity(CBaseEntity *entity)
+    {
+        return derived().processEntity(entity);
+    }
+    void processEntity(CBaseEntity *entity)
+    {
+        return;
+    }
 
-	// called on CBaseGameSystemPerFrame::Update which is called AFTER CL_Move
-	void _perFrame ()
-	{
-		return derived ().perFrame ();
-	}
-	void perFrame ()
-	{
-		return;
-	}
+    // called on CBaseGameSystemPerFrame::Update which is called AFTER CL_Move
+    void _perFrame()
+    {
+        return derived().perFrame();
+    }
+    void perFrame()
+    {
+        return;
+    }
 
-	// called EVERY frame (not just when we are in game)
-	void _everyFrame ()
-	{
-		return derived ().everyFrame ();
-	}
-	void everyFrame ()
-	{
-		return;
-	}
+    // called EVERY frame (not just when we are in game)
+    void _everyFrame()
+    {
+        return derived().everyFrame();
+    }
+    void everyFrame()
+    {
+        return;
+    }
 };
